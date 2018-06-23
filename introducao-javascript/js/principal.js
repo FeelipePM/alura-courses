8d57f29ed95be9175ac601ad1323714ea1aaea4d
Felipe Machado <Feelipe@email.com>
2018-06-23 15:23:55 -0300

    validating imc

--- introducao-javascript/js/principal.js
+++ introducao-javascript/js/principal.js
@@ -4,14 +4,35 @@ titulo.textContent = "Aparecida Nutricionista";
 
 var paciente = document.querySelector("#primeiro-paciente");
 
-var tdPeso = paciente.querySelector(".info-peso");
-var peso = tdPeso.textContent;
-
 var tdAltura = paciente.querySelector(".info-altura");
-var altura = tdAltura.textContent;
-
+var tdPeso = paciente.querySelector(".info-peso");
 var tdImc = paciente.querySelector(".info-imc");
 
-var imc = peso / (altura * altura);
+var altura = tdAltura.textContent;
+var peso = tdPeso.textContent;
 
-tdImc.textContent = imc;
+var alturaEhValida = true;
+var pesoEhValido = true;
+
+if(peso <= 0 || peso >= 1000)   {
+    console.log("Peso Inválido!");
+    tdImc.textContent = "Peso Inválido!";
+    pesoEhValido = false;
+}
+
+if(altura <= 0 || altura  >= 3.00)  {
+    console.log("Altura Inválido!");
+    tdImc.textContent = "Altura Inválido!";
+    alturaEhValida = false;
+}
+
+if(pesoEhValido && alturaEhValida)  {
+    var imc = peso / (altura * altura);
+    tdImc.textContent = imc;
+}
+
+
+
+
+
+
