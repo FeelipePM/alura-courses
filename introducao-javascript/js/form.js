f440f1c3ba5bb1c9c96cb171603f98c92fd2a4ce
Felipe Machado <Feelipe@email.com>
2018-06-28 20:59:22 -0300

    validating form

--- introducao-javascript/js/form.js
+++ introducao-javascript/js/form.js
@@ -5,14 +5,23 @@ botaoAdicionar.addEventListener("click", function(event) {
 
   var form = document.querySelector("#form-adiciona");
   var paciente = obtemPacienteDoFormulario(form);
+  var pacienteTr = montaTr(paciente); 
 
-  var pacienteTr = montaTr(paciente);  
+  var erros = validaPaciente(paciente);
 
+    if (erros.length > 0) {
+        exibeMensagensDeErro(erros);
+        return;
+    }
   var tabela = document.querySelector("#tabela-pacientes");
 
   tabela.appendChild(pacienteTr);
 
   form.reset();
+
+  var mensagensErro = document.querySelector("#mensagens-erro");
+  mensagensErro.innerHTML = "";
+
 });
 
 function obtemPacienteDoFormulario(form) {
@@ -45,4 +54,46 @@ function montaTd(dado, classe) {
     td.classList.add(classe);
 
     return td;
+}
+
+function validaPaciente(paciente) {
+
+    var erros = [];
+
+    if (paciente.nome.length == 0) {
+        erros.push("O nome não pode ser em branco");
+    }
+
+    if (paciente.gordura.length == 0) {
+        erros.push("A gordura não pode ser em branco");
+    }
+
+    if (paciente.peso.length == 0) {
+        erros.push("O peso não pode ser em branco");
+    }
+
+    if (paciente.altura.length == 0) {
+        erros.push("A altura não pode ser em branco");
+    }
+
+    if(!validaPeso(paciente.peso))   {
+        erros.push("Peso é inválido!");
+    }
+
+    if (!validaAltura(paciente.altura)) {
+        erros.push("Altura é inválida!");
+    }
+    return erros;
+}
+
+function exibeMensagensDeErro(erros) {
+    var ul = document.querySelector("#mensagens-erro");
+    ul.innerHTML = "";
+
+    erros.forEach(function(erro) {
+        var li = document.createElement("li");
+        li.textContent = erro;
+        ul.appendChild(li);
+
+    });
 }
