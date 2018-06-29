7c2bca90137b4df538c201b2ec9c16a7ca1574da
Felipe Machado <Feelipe@email.com>
2018-06-29 19:24:29 -0300

    add function remove patient

---
+++ introducao-javascript/js/remover-paciente.js
@@ -0,0 +1,16 @@
+var pacientes = document.querySelectorAll(".paciente");
+
+var tabela = document.querySelector("#tabela-pacientes");
+
+tabela.addEventListener("dblclick", function (event) {
+    event.target.parentNode.classList.add("fadeOut");
+
+    if (event.target.tagName == "TD") {
+        setTimeout(function () {
+            event.target.parentNode.remove();
+        }, 500);
+
+    }
+
+});
+
