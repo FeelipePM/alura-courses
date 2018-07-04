5b142aadb0955b282410c4251ec69885ef698191
Felipe Machado <Feelipe@email.com>
2018-07-04 17:09:46 -0300

    stopwatch and typing field

--- alura-typer/public/js/main.js
+++ alura-typer/public/js/main.js
@@ -1,5 +1,28 @@
 var frase = $(".frase").text();
 var numPalavras = frase.split(" ").length;
-
 var tamanhoFrase = $("#tamanho-frase");
 tamanhoFrase.text(numPalavras);
+
+var campo = $(".campo-digitacao");
+campo.on("input", function () {
+    var conteudo = campo.val();
+    
+    var qtdPalavras = conteudo.split(/\S+/).length -1;
+    $("#contador-palavras").text(qtdPalavras);
+
+    var qtdCaracteres = conteudo.length;
+    $("#contador-caracteres").text(qtdCaracteres);
+
+    });
+
+    var tempoRestante = $("#tempo-digitacao").text();
+    campo.one("focus", function() {
+        var cronometroID = setInterval(function()  {
+            tempoRestante--;
+            $("#tempo-digitacao").text(tempoRestante);
+            if (tempoRestante < 1) {
+                campo.attr("disabled", true);
+                clearInterval(cronometroID);
+            }   
+        }, 1000);
+    });
