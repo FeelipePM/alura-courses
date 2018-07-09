a5272fcb31500e77df15f090708e98218c267aa2
Felipe Machado <Feelipe@email.com>
2018-07-09 17:08:42 -0300

    dividing the code to improve maintenance

--- alura-typer/public/js/main.js
+++ alura-typer/public/js/main.js
@@ -29,48 +29,54 @@ function inicializaContadores() {
     });
 }
 
-    function inicializaCronometro() {
-        var tempoRestante = $("#tempo-digitacao").text();
-        campo.one("focus", function() {
-            $("#botao-reiniciar").attr("disabled",	true);
-            var cronometroID = setInterval(function () {
-                tempoRestante--;
-            $("#tempo-digitacao").text(tempoRestante);
-                if (tempoRestante < 1) {
-                    campo.attr("disabled",	true);
-                    clearInterval(cronometroID);
-                    campo.toggleClass("campo-desativado");
-            $("#botao-reiniciar").attr("disabled",	false);
-                }
-            }, 1000);
-        });
+function inicializaMarcadores() {
+  var frase = $(".frase").text();
+  campo.on("input", function() {
+    var digitado = campo.val();
+    var comparavel = frase.substr(0, digitado.length);
+
+    if (digitado == comparavel) {
+      campo.addClass("borda-verde");
+      campo.removeClass("borda-vermelha");
+    } else {
+      campo.addClass("borda-vermelha");
+      campo.removeClass("borda-verde");
     }
+  });
+}
 
-	function inicializaMarcadores() {
-		var frase = $(".frase").text();
-		campo.on("input", function () {
-			var digitado = campo.val();
-			var comparavel = frase.substr(0, digitado.length);
+function inicializaCronometro() {
+    var tempoRestante = $("#tempo-digitacao").text();
+        campo.one("focus", function() {
+        $("#botao-reiniciar").attr("disabled",	true);
+    var cronometroID = setInterval(function () {
+        tempoRestante--;
+        $("#tempo-digitacao").text(tempoRestante);
+    if (tempoRestante < 1) {
+        campo.attr("disabled", true);
+        clearInterval(cronometroID);
+        $("#botao-reiniciar").attr("disabled", false);
+        finalizaJogo();
+            }
+        }, 1000);
+    });
+}
 
-			if (digitado == comparavel) {
-				campo.addClass("borda-verde");
-				campo.removeClass("borda-vermelha");
-			} else {
-				campo.addClass("borda-vermelha");
-				campo.removeClass("borda-verde");
-			}
-		});
+function finalizaJogo() {
+    campo.attr("disabled", true);
+    campo.toggleClass("campo-desativado");
+    inserePlacar();
 	}
  
-		$("#botao-reiniciar").click(reiniciaJogo);
-    function reiniciaJogo() {
-      campo.attr("disabled",	false);
-    	campo.val("");
-      $("#contador-palavras").text(0);
-      $("#contador-caracteres").text(0);
-			$("#tempo-digitacao").text(tempoInicial);
-      inicializaCronometro();
-			campo.toggleClass("campo-desativado");
-			campo.removeClass("borda-vermelha");
-			campo.removeClass("borda-verde");
-		}
+function reiniciaJogo() {
+    campo.attr("disabled", false);
+    campo.val("");
+    $("#contador-palavras").text(0);
+    $("#contador-caracteres").text(0);
+    $("#tempo-digitacao").text(tempoInicial);
+    inicializaCronometro();
+    campo.toggleClass("campo-desativado");
+    campo.removeClass("borda-vermelha");
+    campo.removeClass("borda-verde");
+}
+       
