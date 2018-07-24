634c2eb1dd29a72b1c4092eae5a9c2c0459be325
Felipe Machado <Feelipe@email.com>
2018-07-23 23:48:04 -0300

    add function sincronizaPlacar

--- alura-typer/public/js/placar.js
+++ alura-typer/public/js/placar.js
@@ -1,4 +1,5 @@
 $("#botao-placar").click(mostraPlacar);
+$("#botao-sync").click(sincronizaPlacar);
 
 function inserePlacar() {
     var corpoTabela = $(".placar").find("tbody");
@@ -54,4 +55,32 @@ function removeLinha() {
 
 function mostraPlacar() {
   $(".placar").stop().slideToggle(600);
+}
+
+function sincronizaPlacar() {
+    var placar = [];
+    var linhas = $("tbody>tr");
+
+    linhas.each(function() {
+        var usuario = $(this).find("td:nth-child(1)").text();
+        var palavras = $(this).find("td:nth-child(2)").text();
+
+        var score = {
+            usuario: usuario,
+            pontos: palavras
+        };
+
+        placar.push(score);
+
+    });
+
+    var dados = {
+        placar: placar
+    };
+
+    $.post("http://localhost:3000/placar", dados, function() {
+        console.log("Enviado com Sucesso!");
+        
+    });
+
 }
