653aed8192f6fb0dc2ec2a83b5537ad93bda9826
Felipe Machado <Feelipe@email.com>
2018-07-27 19:08:01 -0300

    animation for button

---
+++ design-responsivo/js/menu.js
@@ -0,0 +1,14 @@
+document.querySelector(".menu-abrir").onclick = function() {
+    document.documentElement.classList.add("menu-ativo");
+};
+
+document.querySelector('.menu-fechar').onclick = function () {
+    document.documentElement.classList.remove('menu-ativo');
+};
+
+document.documentElement.onclick = function(event) {
+    if(event.target === document.documentElement){
+        document.documentElement.classList.remove("menu-ativo");
+    } 
+};
+
