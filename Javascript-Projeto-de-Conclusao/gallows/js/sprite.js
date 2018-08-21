32c9de0e3f20e3059581982884a00d3c1ad9caeb
Felipe Machado <Feelipe@email.com>
2018-08-21 16:32:06 -0300

    criado a lógica do boneco na forca

--- Javascript-Projeto-de-Conclusao/gallows/js/sprite.js
+++ Javascript-Projeto-de-Conclusao/gallows/js/sprite.js
@@ -1,26 +1,43 @@
-function createSprite(selector) {
-  var $elemento = $(selector);
+var createSprite = function(selector) {
 
-  var frames = [
-    'frame1', 'frame2', 'frame3', 'frame4', 'frame5', 
-    'frame6', 'frame7', 'frame8', 'frame9', 
-  ];
+var  hasNext = function() {
+  return current + 1 <= last;
+};
 
-  var current = 0;
+var moveFrame = function(from, to) {
+  $elemento.removeClass(from)
+            .addClass(to);
+};
 
-  var last = frames.length -1;
+var nextFrame = function() {
+  if (hasNext()) {
+    moveFrame (frames[current], frames[++current]);
+  };
+}; 
 
-  $elemento.addClass(frames[current]);
+var reset = function() {
+  moveFrame(frames[current], frames[0]);
+  current = 0;
+};
 
-  function moveFrame(from, to) {
-    $elemento.removeClass(from)
-             .addClass(to);
-  }
+var isFinished = function() {
+  return !hasNext();
+};
 
-  function nextFrame() {
-    moveFrame(frames[current], frames[++current]);
-  }
-    return {
-      nextFrame: nextFrame
-    };
-}
+var $elemento = $(selector);
+
+var frames = [
+  'frame1', 'frame2', 'frame3', 'frame4', 'frame5', 
+  'frame6', 'frame7', 'frame8', 'frame9', 
+];
+
+var current = 0;
+
+var last = frames.length -1;
+
+$elemento.addClass(frames[current]);
+
+  return { nextFrame: nextFrame,
+            reset: reset,
+            isFinished: isFinished };
+};
